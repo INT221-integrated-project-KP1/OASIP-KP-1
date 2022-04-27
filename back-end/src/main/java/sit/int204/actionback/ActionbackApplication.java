4e509fba4b24f0a7486a24263ad0656f93c95422
tuskingcup <tuskung8030@gmail.com>
2022-04-27 19:37:03 +0700

    link back and db

--- back-end/src/main/java/sit/int204/actionback/ActionbackApplication.java
+++ back-end/src/main/java/sit/int204/actionback/ActionbackApplication.java
@@ -3,10 +3,13 @@ package sit.int204.actionback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.beans.BeanProperty;
+
 @SpringBootApplication
 public class ActionbackApplication {
     public static void main(String[] args) {
         SpringApplication.run(ActionbackApplication.class, args);
     }
+
 
 }
