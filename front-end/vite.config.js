990df5448af17a6a3447eedefb02de522331d6bb
tuskingcup <tuskung8030@gmail.com>
2022-04-30 01:29:49 +0700

    commit check

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -3,14 +3,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    // server: {
-    //     proxy: {
-    //         '/api': {
-    //             target: `http://${back}:5000`,
-    //             changeOrigin: true,
-    //             secure: false,
-    //             rewrite: (path) => path.replace(/^\/api/, '')
-    //         }
-    //     }
-    // }
+    server: {
+        proxy: {
+            '/api': {
+                target: `http://10.4.56.84:5000`,
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
+    }
 })
