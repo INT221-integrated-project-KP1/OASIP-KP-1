c00fbf23ec478b5fa8c86f4832bba1d012c04acc
tuskingcup <tuskung8030@gmail.com>
2022-04-30 18:55:41 +0700

    test action

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -3,14 +3,30 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    server: {
-        proxy: {
-            '/api': {
-                target: `http://10.4.56.84:5000`,
-                changeOrigin: true,
-                secure: false,
-                rewrite: (path) => path.replace(/^\/api/, '')
-            }
-        }
-    }
+    // server: {
+    //     proxy: {
+    //         '/foo': 'http://localhost:4567',
+    //         "^/api": {
+    //             target: "http://10.4.56.84:5000",
+    //             changeOrigin: true,
+    //             secure: false,
+    //             rewrite: (path) => path.replace(/^\/api/, ''),
+    //             ws: true,
+    //         }
+    //     }
+    // }
 })
+
+// module.exports={
+//     devServer: {
+//         proxy: {
+//           "^/api/": {
+//             target: "http://api:3000",
+//             secure: false,
+//             pathRewrite: {
+//               "/api/*": "/"
+//             }
+//           }
+//         }
+//       }
+//     };
