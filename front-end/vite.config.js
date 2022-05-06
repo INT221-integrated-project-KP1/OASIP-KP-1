2d0d99f77dc5f6773a185d76f8fab36be064ccbc
tuskingcup <tuskung8030@gmail.com>
2022-05-06 22:16:47 +0700

    add /kp1

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     server: {
         proxy: {
             "^/api": {
-                target: 'http://localhost/api',
+                target: 'http://intproj21.sit.kmutt.ac.th/kp1/api',
                 changeOrigin: true,
                 secure: false,
                 rewrite: (path) => path.replace(/^\/api/, ''),
