1a1bb20f4346b346bf97b8343bf371e028b1a50a
tuskingcup <tuskung8030@gmail.com>
2022-05-06 23:33:23 +0700

    change url

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -15,13 +15,7 @@ export default defineConfig({
                 rewrite: (path) => path.replace(/^\/api/, ''),
                 ws: true,
             },
-            "^/": {
-                target: '/kp1',
-                changeOrigin: true,
-                secure: false,
-                rewrite: (path) => path.replace(/^\//, ''),
-                ws: true,
-            }
+
         }
     }
 })
