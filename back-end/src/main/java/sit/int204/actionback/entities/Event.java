39dd33ec7fac016896d4911395800df7cc00bab2
markid1ggwp <markmopairat12345@gmail.com>
2022-05-09 03:15:49 +0700

    edit srcipt v2

--- back-end/src/main/java/sit/int204/actionback/entities/Event.java
+++ back-end/src/main/java/sit/int204/actionback/entities/Event.java
@@ -7,7 +7,7 @@ import java.time.Instant;
 @Table(name = "event")
 public class Event {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "booking_id", nullable = false)
     private Integer id;
 
