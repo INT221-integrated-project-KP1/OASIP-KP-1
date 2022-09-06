e663b1864ab8cfc53b10db6188cddcb7509b324d
tuskingcup <tuskung8030@gmail.com>
2022-09-06 21:02:14 +0700

    add cer

--- front-end/cypress/support/commands.js
+++ front-end/cypress/support/commands.js
@@ -23,3 +23,11 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add(
+    "shouldHaveText",
+    { prevSubject: true },
+    (subject, equalTo) => {
+      expect(subject.text().trim()).to.equal(equalTo);
+    }
+  );
