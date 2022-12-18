5f34f8d23be79f812f8bb182339342c6756a5d6d
Tus <54474594+tuskingcup@users.noreply.github.com>
2022-12-18 23:22:06 +0700

    add transition hover

--- front-end/src/components/LogIn.vue
+++ front-end/src/components/LogIn.vue
@@ -343,8 +343,8 @@ const loginMS = async () => {
 										v-model="loginuser.password"
 									/>
 								</div>
-								<div id="button" class="flex flex-col w-full my-5">
-									<div class="grid grid-cols-2">
+								<div id="button" class="flex flex-col w-full my-5 ">
+									<div class="grid grid-cols-2 gap-7">
                                         <button
 											@click="loginMS"
 											class="bg-base-300 hover:bg-secondary transition-all text-black font-bold py-2 px-4 gap-4 rounded-md flex"
@@ -364,9 +364,9 @@ const loginMS = async () => {
 										</button>
 										<button
 											type="button"
-											class="w-full py-4 bg-green-600 rounded-lg text-green-100"
+											class="bg-base-300 w-full py-4 bg-green-600 rounded-lg text-green-100 hover:bg-secondary transition-all"
 										>
-											<div class="flex flex-row items-center justify-center">
+											<div class="flex flex-row items-center justify-center ">
 												<div class="mr-2">
 													<svg
 														class="w-6 h-6"
@@ -384,7 +384,7 @@ const loginMS = async () => {
 													</svg>
 												</div>
 												<div
-													class="font-bold"
+													class="font-bold "
 													@click="MatchingCheck(loginuser)"
 												>
 													Sign in
@@ -397,13 +397,13 @@ const loginMS = async () => {
 									<div class="flex justify-evenly mt-5">
 										<a
 											href="#"
-											class="w-full text-center font-medium text-gray-500"
+											class="w-full text-center font-medium text-gray-500 hover:text-secondary transition-all"
 											@click="noIsFun"
 											>Recover password!</a
 										>
 										<a
 											href="#"
-											class="w-full text-center font-medium text-gray-500"
+											class="w-full text-center font-medium text-gray-500 hover:text-secondary transition-all"
 											@click="goSignUp"
 											>Sign up!</a
 										>
